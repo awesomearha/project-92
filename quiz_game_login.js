41719dc2d768c3a20201f82763d7180b05b181a3
awesomearha <138312704+awesomearha@users.noreply.github.com>
2024-03-19 09:27:26 +0400

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,16 @@
+function addUser() {
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.getItem("player2_name", player2_name);
+
+    window.location.replace("game_page.html");
+}
+
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+
+
